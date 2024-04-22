398dbc27a0bc5530da3696a97cf6ccaec154141f
Romain <romain.vlrt@gmail.com>
2024-04-22 16:15:19 +0200

    clean

--- Transcendance_Back/Transcendance/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Message, User, Conversation, GameStats, GameHistory, PFC_Game_ID
-admin.site.register(Message)
-admin.site.register(User)
-admin.site.register(Conversation)
-admin.site.register(GameStats)
-admin.site.register(GameHistory)
-admin.site.register(PFC_Game_ID)
--- Transcendance_Back/Transcendance/management/commands/erase_users.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-from Transcendance.models import User
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        User.objects.all().delete()
-        print("Toutes les données de la table User ont été supprimées avec succès ✅")
